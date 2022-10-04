90fc38b626314e3e17cd93a14603b4986ca79242
TCHF25 <70825865+TCHF25@users.noreply.github.com>
2022-10-04 22:32:30 +0300

    add Api for sign up with Jwt auth

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,7 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['middleware'=>'api', 'prefix' => 'auth'], function ($router) {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
 });
